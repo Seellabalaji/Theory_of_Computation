faff979a8efd94ca3d93582b2b6e7f3b1ca298df
Seellabalaji <113584746+Seellabalaji@users.noreply.github.com>
2023-08-24 10:08:55 +0530

    Create Exp no. 07(S->A101A    A->0A|1A|e).c

---
+++ Exp no. 07(S->A101A    A->0A|1A|e).c
@@ -0,0 +1,44 @@
+//Accept string with substring 101
+// S->A101A    A->0A|1A|e
+
+
+#include<stdio.h>
+#include<string.h>
+int main()
+{
+char s[100];
+int i,flag,flag1;
+int l;
+printf("CONTEXT FREE GRAMMER : S->A101A    A->0A|1A|e\n");
+printf("Enter a string to check:");
+scanf("%s",s);
+l=strlen(s);
+flag=1;
+for(i=0;i<l;i++)
+{
+if(s[i]!='0' && s[i]!='1')
+{
+ flag=0;
+}
+}
+if(flag==1)
+{
+flag1=0;
+for(i=0;i<l-2;i++)
+{
+if(s[i]=='1')
+{
+if(s[i+1]=='0' && s[i+2]=='1')
+{
+flag1=1;
+printf("Substring 101 exists.\n String accepted\n");
+break;
+}
+}
+}
+if(flag1==0)
+printf("Substring 101 does not exist.\n String not accepted\n");
+}
+else
+printf("string is Not Valid\n");
+}
